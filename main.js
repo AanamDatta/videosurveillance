24f0bc9618fe6b0a59d15334d88c0ba740440e92
AanamDatta <78989806+AanamDatta@users.noreply.github.com>
2022-11-18 20:43:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+    video = createVideo("video.mp4")
+   
+}
+
+function setup() {
+    canvas = createCanvas(500,400)
+    canvas.center()
+    video.hide()
+}
+function draw(){
+    image(video,0,0,500,400)
+}
